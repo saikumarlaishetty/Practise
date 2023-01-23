885719eaf6f964095669677eddfd09a226bfb494
slaishet <slaishet@cisco.com>
2023-01-23 17:33:45 +0530

    Tuple methods

---
+++ Python_Built_in_functions/Tuple_Methods.py
@@ -0,0 +1,10 @@
+# count() - Returns the number of times a specified value occurs in a tuple
+thistuple = (1, 3, 7, 8, 7, 5, 4, 6, 8, 5)
+x = thistuple.count(5)
+print(x)
+
+# index() - searches the tuple for a specified value and returns the postion of where it was found.
+print(thistuple.index(8))
+
+
+

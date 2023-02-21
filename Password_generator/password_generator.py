a2995995ae46791e70315d8d98f1fda02816eee7
slaishet <slaishet@cisco.com>
2023-02-21 09:28:27 +0530

    Added password generator

---
+++ Password_generator/password_generator.py
@@ -0,0 +1,44 @@
+import random
+import string
+
+def generate_password(min_length,numbers=True,special_characters=True):
+
+    letters = string.ascii_letters
+    digits = string.digits
+    special = string.punctuation
+
+    characters = letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
+
+    pwd = ""
+    meets_criteria = False
+    has_number = False
+    has_special = False
+
+    while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+            has_special = True
+
+        meets_criteria = True
+        if numbers:
+            meets_criteria = has_number
+        if special_characters:
+            meets_criteria = meets_criteria and has_special
+
+    return pwd
+
+
+min_length = int(input("Enter the minimu length: "))
+has_numbers = input("Do you want to have numbers (y/n)?").lower() == 'y'
+has_special = input("Do you wan to have special Charactars (y/n) ?").lower() == 'y'
+pwd = generate_password(min_length,has_numbers,has_special)
+print("The generated password is:",pwd)
+
